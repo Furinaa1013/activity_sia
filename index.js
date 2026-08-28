a13cc9e7df5a927f41b7d07222a8c2476734c356
unknown <pc7@csitlab.local>
2026-08-28 11:25:11 +0800

    feat: add express server setup

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const furina = require('express')
 const server = furina()
 const PORT = 1013
 const HOSTNAME = '0.0.0.0'
-furina.listen(PORT, HOSTNAME() => {
-console.log(`Server is running in ${HOSTNAME}:${PORT}`)
+server.listen(PORT, HOSTNAME, () => {
+     console.log(`Server is running in ${HOSTNAME}:${PORT}`)
 })
